f0f42015d92d7a6a516ae8e26a8426c8eeff2777
Zadigo <pendenquejohn@gmail.com>
2023-04-13 15:27:45 +0200

    Persist to data file

--- neptunia/utils.py
+++ neptunia/utils.py
@@ -3,7 +3,7 @@ import json
 
 
 def write_file(filename, value, filetype='csv'):
-    with open(f'neptunia/data/{filename}', mode='a+', encoding='utf-8') as f:
+    with open(f'neptunia/data/{filename}', mode='a+', encoding='utf-8-sig') as f:
         result = dict(value)
         f.write(json.dumps(result))
         # result = json.load()
