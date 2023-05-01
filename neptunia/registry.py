8fc8d4a4cf0f4feedf6b83d94106f913fb044026
Zadigo <pendenquejohn@gmail.com>
2023-05-01 17:25:11 +0200

    Improve registry

--- neptunia/registry.py
+++ neptunia/registry.py
@@ -1,6 +1,25 @@
 class AppRegistry:
-    def load_registry(self):
+    """Loads all the required applications
+    for running the web scrapper"""
+
+    def __init__(self):
+        self.cache = None
+        self.middlewares = None
+        self.app_config = None
+        self.storage = None
+        self.is_ready = False
+
+    def configure_cache(self):
+        # TODO: Load the registry in the
+        # current application cache
         pass
+
+    def load(self):
+        self.is_ready = True
+
+    def run_scrapper(self, url, **kwargs):
+        if not self.is_ready:
+            raise ValueError("Registry is not ready")
 
 
 registry = AppRegistry()
